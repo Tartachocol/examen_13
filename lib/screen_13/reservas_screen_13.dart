8059b554365d5a37637a1ccdf172f4e7bf3304d9
Tartachocol <flcara278@ieszaidinvergeles.org>
2022-12-19 20:27:25 +0100

    No cambia de boton

--- lib/screen_13/reservas_screen_13.dart
+++ lib/screen_13/reservas_screen_13.dart
@@ -1,44 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-class ReservasScreen_13 extends StatelessWidget {
+class ReservasScreen_13 extends StatefulWidget {
   const ReservasScreen_13({Key? key}) : super(key: key);
 
   @override
+  State<ReservasScreen_13> createState() => _ReservasScreen_13State();
+}
+
+class _ReservasScreen_13State extends State<ReservasScreen_13> {
+  @override
   Widget build(BuildContext context) {
-    diaSemana(int day) {
-      switch (day) {
-        case 1:
-          return ('L');
-          break;
-
-        case 2:
-          return ('M');
-          break;
-
-        case 3:
-          return ('X');
-          break;
-
-        case 4:
-          return ('J');
-          break;
-
-        case 5:
-          return ('V');
-          break;
-
-        case 6:
-          return ('S');
-          break;
-        case 7:
-          return ('D');
-          break;
-        default:
-          return ('Day Not Found');
+    ElevatedButton muestraBoton(pulsado) {
+      if (pulsado) {
+        return ElevatedButton(
+          style: ElevatedButton.styleFrom(primary: Colors.grey),
+          child: const Text('Cancelar'),
+          onPressed: () {
+            pulsado = false;
+            print(pulsado);
+          },
+        );
+      } else {
+        return ElevatedButton(
+          child: const Text('Reservar'),
+          onPressed: () {
+            pulsado = true;
+            print(pulsado);
+          },
+        );
       }
     }
 
-    String? pulsado;
+    DateTime _selectedDay = DateTime.now();
+
+    DateTime _focusedDay = DateTime.now();
+    bool pulsado = false;
     return Scaffold(
       appBar: AppBar(),
       body: Center(
@@ -47,178 +45,47 @@ class ReservasScreen_13 extends StatelessWidget {
             Container(
               color: Colors.grey[300],
               child: Column(children: [
-                Text(
-                  textAlign: TextAlign.center,
-                  DateUtils.dateOnly(DateTime.now()).month.toString(),
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 16),
+                TableCalendar(
+                  headerStyle: const HeaderStyle(
+                      formatButtonVisible: false, titleCentered: true),
+                  startingDayOfWeek: StartingDayOfWeek.monday,
+                  calendarFormat: CalendarFormat.week,
+                  firstDay: DateTime.now(),
+                  lastDay: DateTime.utc(2030, 3, 14),
+                  focusedDay: _focusedDay,
+                  selectedDayPredicate: (day) => _selectedDay == day,
+                  onDaySelected: (selectedDay, focusedDay) {
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      _focusedDay = focusedDay;
+                    });
+                  },
                 ),
-                Row(children: [
-                  Expanded(
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Column(
-                        children: [
-                          const Text(
-                            'L',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                          Text(
-                            DateTime.now()
-                                .subtract(const Duration(days: 1))
-                                .day
-                                .toString(),
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      child: Column(
-                        children: [
-                          const Text(
-                            'M',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                          Text(
-                            DateTime.now()
-                                .subtract(const Duration(days: 1))
-                                .day
-                                .toString(),
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                        ],
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Column(
-                        children: [
-                          const Text(
-                            'X',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                          Text(
-                            DateTime.now().day.toString(),
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      child: Column(
-                        children: [
-                          const Text(
-                            'J',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                          Text(
-                            DateTime.now()
-                                .subtract(const Duration(days: 1))
-                                .day
-                                .toString(),
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                        ],
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Column(
-                        children: [
-                          const Text(
-                            'V',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                          Text(
-                            DateTime.now().day.toString(),
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const Text(
-                            'S',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                          Text(
-                            DateTime.now()
-                                .add(const Duration(days: 14))
-                                .day
-                                .toString(),
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                        ],
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      child: Column(
-                        children: [
-                          const Text(
-                            'D',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                          Text(
-                            DateTime.now().day.toString(),
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                        ],
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                ]),
               ]),
             ),
             Card(
               margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
               child: Column(mainAxisSize: MainAxisSize.min, children: [
-                Container(
-                  child: const Text(
-                    softWrap: true,
-                    textAlign: TextAlign.start,
-                    '9:00 / 10:00',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Text(
+                      textAlign: TextAlign.start,
+                      '9:00 / 10:30',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    Text(
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      'Pista de padel',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                  ],
                 ),
-                const Text(
-                  softWrap: true,
-                  textAlign: TextAlign.center,
-                  'Pista de padel',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                const SizedBox(
+                  height: 20,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -227,28 +94,7 @@ class ReservasScreen_13 extends StatelessWidget {
                     Text('duracion : 1h 30 min'),
                   ],
                 ),
-
-                /*  Builder(
-                  builder: (context) {
-                    if (pulsado!.length >= 0) {
-                      return TextButton(
-                        child: const Text('Cancelar'),
-                        onPressed: () {
-                          pulsado = "";
-                          print(pulsado);
-                        },
-                      );
-                    } else {
-                      return TextButton(
-                        child: const Text('Reservar'),
-                        onPressed: () {
-                          pulsado = "true";
-                          print(pulsado);
-                        },
-                      );
-                    }
-                  },
-                ) */
+                Container(child: muestraBoton(pulsado)),
               ]),
             ),
           ],
